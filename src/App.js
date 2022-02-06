1af0d235274518ab7b5e46ed6099b1cc24435ff6
MIRZA ZIYAD AHMED BAIG <beygz17@gmail.com>
2022-02-06 23:21:32 +0500

    attendance-mngmt

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import AddStudenthtmlForm from './Components/AddStudentForm';
+import Nav from './Components/Nav'
+
+function App() {
+  return <div>
+    <Nav/>
+    <AddStudenthtmlForm/>
+  
+    
+  </div>;
+}
+
+export default App;
